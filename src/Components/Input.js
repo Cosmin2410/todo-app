bea7b4b6b994ea806e5ddfa299f19c08913bb049
Cosmin2410 <cosminnae00@yahoo.com>
2022-05-22 23:56:17 +0300

    todo app

---
+++ src/Components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input() {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add Task</button>
+    </div>
+  );
+}
+
+export default Input;
---
+++ src/Components/Task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import icon from '../../public/images/x.svg';
+
+function Task() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <input type="checkbox" /> task one <img src="" />
+        </li>
+        <li>task two</li>
+        <li>task three</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Task;
